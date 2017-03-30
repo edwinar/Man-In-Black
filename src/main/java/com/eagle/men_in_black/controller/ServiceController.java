62b9fc94b273d171befd5ba023226f2370e8aa1e
zexoper999 <zexoper@naver.com>
2017-03-30 11:34:27 +0900

    service , notice 커밋

--- src/main/java/com/eagle/men_in_black/controller/ServiceController.java
+++ src/main/java/com/eagle/men_in_black/controller/ServiceController.java
@@ -3,8 +3,20 @@ package com.eagle.men_in_black.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ServiceController {
 	Logger loger = LoggerFactory.getLogger(this.getClass());
+	
+	
+	@RequestMapping("servicenotice.mib")
+	public ModelAndView coupon_Administer() {
+
+		ModelAndView mav = new ModelAndView("service/notice");
+		
+		return mav;
+
+	}
 }
