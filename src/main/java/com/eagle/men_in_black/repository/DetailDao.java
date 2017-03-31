e3ff6e433eb7a5a1a29d3ce9bba3b5dfa3ea703f
InBaeHwang <hinbae2002@naver.com>
2017-03-31 12:25:11 +0900

    인배 쿠키까지

--- src/main/java/com/eagle/men_in_black/repository/DetailDao.java
+++ src/main/java/com/eagle/men_in_black/repository/DetailDao.java
@@ -1,5 +1,10 @@
 package com.eagle.men_in_black.repository;
 
-public interface DetailDao {
+import java.util.List;
 
+import com.eagle.men_in_black.model.DetailDto;
+
+public interface DetailDao {
+	//상품 상세정보
+	public List<DetailDto> do_selectProductDetail(int PRO_SEQ);
 }
